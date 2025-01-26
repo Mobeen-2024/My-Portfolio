1050debe517991ed92f314b8c82ae59a2d6ba589
Mobeen-2014 <m.mobeen.2024@gmail.com>
2025-01-26 10:11:41 +0500

    Having issue with many files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    container: {
+      center: true,
+      padding: "15px",
     },
-  },
-  plugins: [],
-};
+	screens: {
+	  sm: '640px',
+	  md: '768px',
+	  lg: '960px',
+	  xl: '1200px'
+	},
+	fontFamily: {
+		primary: "var(--font-jertbrainsMono)",
+	},
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+			form: { height: "var(--radix-accordion-content-height)" },
+			to: { height: "0" },
+        }
+	},
+		animation: {
+		  "accoedion-down": "accordation-down 0,2s ease-out",
+		  "accoedion-up": "accordation-up 0,2s ease-out"
+		},
+      },
+	},
+	plugins: [require("tailwindcss-animate")],
+}

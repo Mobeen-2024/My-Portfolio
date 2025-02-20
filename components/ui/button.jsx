14825de84a269c706903a134174f7f9a1b5a2033
VeNom <m.mobeen.2024@gmail.com>
2025-02-20 16:58:19 +0500

    Contact section added and working

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
+        sm: "h-8 rounded-md px-4 py-5 text-xs",
         lg: "h-10 rounded-md px-8 text-sm uppercase tracking-[2px]",
         icon: "h-9 w-9",
       },

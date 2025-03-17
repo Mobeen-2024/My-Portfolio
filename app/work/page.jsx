251018bcb55a4ae6b276fdf1bfabd976346aac67
Mobeen-2024 <m.mobeen.2024@gmail.com>
2025-03-17 17:43:35 +0500

    The change in my personal information

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,10 +1,16 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
@@ -15,8 +21,8 @@ import {
   TooltipTrigger,
 } from "@radix-ui/react-tooltip";
 
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
@@ -157,14 +163,24 @@ const work = () => {
                       <div className='absolute top-0 bottom-0 w-full h-full bg-black/10 z-10'></div>
 
                       {/* image  */}
-                      <div>
-                        <Image
-                          src={project.Image}
-                          fill
-                          className='object-cover'
-                          alt='Image'
-                        />
-                      </div>
+                      <Swiper
+                        direction={"vertical"}
+                        pagination={{
+                          clickable: true,
+                        }}
+                        modules={[Pagination]}
+                        className='mySwiper'
+                      >
+                        <SwiperSlide>Slide 1</SwiperSlide>
+                        <SwiperSlide>Slide 2</SwiperSlide>
+                        <SwiperSlide>Slide 3</SwiperSlide>
+                        <SwiperSlide>Slide 4</SwiperSlide>
+                        <SwiperSlide>Slide 5</SwiperSlide>
+                        <SwiperSlide>Slide 6</SwiperSlide>
+                        <SwiperSlide>Slide 7</SwiperSlide>
+                        <SwiperSlide>Slide 8</SwiperSlide>
+                        <SwiperSlide>Slide 9</SwiperSlide>
+                      </Swiper>
                     </div>
                   </SwiperSlide>
                 );
